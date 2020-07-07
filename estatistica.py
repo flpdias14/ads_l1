af7a72691ea14df6a372081fb371f96bf6d990d5
Felipe Oliveira <flpdias14@gmail.com>
2020-07-07 20:25:28 -0300

    histograma cpus

--- estatistica.py
+++ estatistica.py
@@ -6,6 +6,11 @@ import numpy as np
 
 data = pd.read_csv("log_dados.csv", delimiter = ';',encoding='utf-8')
 
+
+hist_plot = data['cpu_em_uso'].hist(bins=25, grid=False)
+hist_plot.set_title('Histograma de CPUs')
+hist_plot.set_xlabel('CPU')
+hist_plot.set_ylabel('# Ocorrências')
+plt.show()
 # print(data)
 
-print(data['cpu_em_uso'].groupby(['cpu_em_uso']).count().unstack('cpu_em_uso'))
